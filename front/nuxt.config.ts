56502108127f7deff29aab0535de632c0a4435c3
Nisrine Maruan <85900780+nisrinemr1@users.noreply.github.com>
2022-10-26 14:57:54 +0200

    init style + form

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
+    css:['~/assets/style/global.scss'],
+
+    vite: {
+        css: {
+        postcss: null,
+        preprocessorOptions: {
+            scss: {
+            additionalData: `@import '~/assets/style/init/variables.scss';`,
+            },
+        },
+        },
+    },
+
 })
---
+++ front/pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="home-page">
+        <h1>Enigma game</h1>
+
+        <div v-for="data in enigma" :key="data">
+
+            <p>Enigma {{data.index}} by {{data.user}}</p>
+
+        </div>
+
+        <form @submit.prevent="submit">
+
+            <input type="text" v-model="enigma.user" placeholder="User name">
+            <input type="text" v-model="enigma.message" placeholder="Your message">
+
+            <button> Send </button>
+
+        </form>
+        
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+
+        data() {
+            return {
+                enigma: [
+                    {user: "Niiiiiah", message: "hello"},
+                ]
+            }
+        },
+
+        methods: {
+
+            submit(){
+                if (!this.enigma.user && !this.enigma.message) {
+                    alert('ERROR 1!!!');
+                }
+
+                else if(!this.enigma.user || !this.enigma.message){
+                    alert("An input is missing -_-");
+                }
+                
+                else{
+                    this.enigma.push({
+                        user: this.enigma.user,
+                        message: this.enigma.message
+                    }
+                    )
+                }
+            }
+
+        }
+
+    }
+</script>
