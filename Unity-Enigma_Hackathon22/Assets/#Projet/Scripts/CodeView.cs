9c93bfa7ce0f6d834c94b7c7c339b7fdec0394d2
Scheherazad Zourhbat <scheherazad.zourhbat@gmail.com>
2022-10-26 16:36:15 +0200

    buttons

--- Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/CodeView.cs
+++ Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/CodeView.cs
@@ -7,10 +7,24 @@ public class CodeView : MonoBehaviour
 {
 
     public GameObject codeViewUI;
+    public Transform cube; 
+
+    void Start() {
+
+        //positionner le boutton par rapport au cube (on doit le faire par rapport à son anchored position)
+        ((RectTransform)transform).anchoredPosition = cube.position;
+
+
+    }
+
+
     public void PushToViewCode()
     {
+        
         if(codeViewUI.activeInHierarchy == true){
             codeViewUI.SetActive(false);
+            
+
         }
         else codeViewUI.SetActive(true);
     }
