ab41a0e05281e448a4f59ea23c614f39f6aede20
Man <manon.paulus@hotmail.com>
2022-10-27 16:40:46 +0200

    last changes

--- Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/flyMove.cs
+++ Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/flyMove.cs
@@ -10,7 +10,6 @@ public class flyMove : MonoBehaviour
     private InputAction moveAction;
     private PlayerInputAction actions; 
     public float moveSpeed = 10f;
-    private float verticalSpeed;
     private CharacterController controller; 
 
     public Camera cameraPlayer; 
@@ -31,18 +30,16 @@ public class flyMove : MonoBehaviour
     {
 
         Vector2 moveDirection = moveAction.ReadValue<Vector2>();
-        
-
-        
+    
         Vector2 movement = moveDirection * moveSpeed * Time.fixedDeltaTime;
 
-        
-        controller.Move(movement.y *cameraPlayer.transform.forward + movement.x * transform.right ); 
+        controller.Move(movement.y *cameraPlayer.transform.forward + movement.x * transform.right); 
         //movement.x c'est pour les flèches gauches et droite
         //movement.y c'est pour les flèches haut et bas
         //cameraPlayer.transform.forward, ça suit le devant de la caméra (ça renvoit un vector3) donc si on multplie par le résultat des flèches du haut, on avance vers là où regarde la caméra
-        //transform.right on aurait pu aussi choisir left, mais c'est pour que ça renvoit les directions latérales du player (ça rSenvoit aussi un vector3) et on le multplie par les inputs des flèches latérales
+        //transform.right on aurait pu aussi choisir left, (mais c'est plus simple de choisir un vector3 positif) mais c'est pour que ça renvoit les directions latérales du player (ça renvoit aussi un vector3) et on le multplie par les inputs des flèches latérales
         //on additionne les deux pour un déplacement complet. 
+    
 
         
         
