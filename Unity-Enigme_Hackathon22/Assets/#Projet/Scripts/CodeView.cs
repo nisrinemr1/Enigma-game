5d2a32273fffb03efbe16645708ef8f12e504ffc
ShailaDevi Mahaux <shailadevi_mahaux@yahoo.fr>
2022-10-27 16:48:24 +0200

    correction Button cliquable

--- Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/CodeView.cs
+++ Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/CodeView.cs
@@ -7,28 +7,55 @@ public class CodeView : MonoBehaviour
 {
     public GameObject codeViewUI;
     private GameObject mainCamera;
+    private Vector3 betweenBoxPlayer;
+    public CubeBehaviour cube;
+    private Canvas canva;
 
-    void Start() {
+    private float extendMax = 0f;
+
+
+    void Start()
+    {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        canva = transform.parent.GetComponent<Canvas>();
+        canva.worldCamera = mainCamera.GetComponent<Camera>();
     }
-    void Update(){
-        
-        transform.LookAt(mainCamera.transform.position);
-        Debug.DrawRay(transform.position, transform.forward,Color.red);
-        transform.parent.forward = mainCamera.transform.position;
+    void Update()
+    {
+        transform.LookAt(mainCamera.transform.position); // orientation du boutton
+        transform.parent.LookAt(mainCamera.transform.position); // orientation du plane dataCube
+
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+
+        float newExtendMax = Mathf.Max(cube.cubeCollider.bounds.extents.x, cube.cubeCollider.bounds.extents.y, cube.cubeCollider.bounds.extents.z);
+        extendMax = Mathf.Max(newExtendMax, extendMax);
+        betweenBoxPlayer = (mainCamera.transform.position - cube.transform.position).normalized;
+        transform.position = cube.transform.position + (betweenBoxPlayer * extendMax) + transform.forward * 0.5f; //position du boutton
+        transform.Rotate(Vector3.up, 180);
     }
+
 
 
     public void PushToViewCode()
     {
-        
-        if(codeViewUI.activeInHierarchy == true){
+        Debug.Log("qierughliuergmh");
+        if (codeViewUI.activeInHierarchy == true)
+        {
             codeViewUI.SetActive(false);
-            
-
         }
         else codeViewUI.SetActive(true);
     }
+
+
+    void OnDrawGizmos()
+    {
+        Color color = Color.cyan;
+        color.a = 0.25f;
+        Gizmos.color = color;
+        Gizmos.DrawWireCube(cube.cubeCollider.bounds.center, Vector3.one * extendMax * 2);
+        Gizmos.DrawCube(cube.cubeCollider.bounds.center, Vector3.one * extendMax * 2);
+    }
+
 
 
 }
--- Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/CubeBehaviour.cs
+++ Unity-Enigme_Hackathon22/Assets/#Projet/Scripts/CubeBehaviour.cs
@@ -9,21 +9,18 @@ public class CubeBehaviour : MonoBehaviour
 
     [HideInInspector] public float rotationSpeed = 50f;
     public TextMeshProUGUI transformCube;
-    private BoxCollider cubeCollider;
+    public BoxCollider cubeCollider;
 
     //Color32 va me donner en 32 bit le format rgb d'un objet
     private Color32 colorCube;
     void Start()
     {
-        cubeCollider = GetComponent<BoxCollider>();
         colorCube = gameObject.GetComponent<MeshRenderer>().material.color;
         transformCube.text = $"{transform.position}  \n {transform.localScale} \n {colorCube.r+ " "+colorCube.g+ " "+colorCube.b}";
     }
     void Update()
     {
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-        
-        
     }
 
 }
