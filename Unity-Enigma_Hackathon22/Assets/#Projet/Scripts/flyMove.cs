86c4dd0300d386e493ea0380521dea85e73e8cf7
ShailaDevi Mahaux <shailadevi_mahaux@yahoo.fr>
2022-10-27 11:05:30 +0200

    flymode

--- Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/flyMove.cs
+++ Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/flyMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class flyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float moveSpeed = 10;
+    //Define the speed at which the object moves.
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        // if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.z)))
+        // {
+        //     //transform
+        // }
+
+        // if (Input.GetKey(KeyCode.DownArrow))
+        // {
+        //     print("down arrow key is held down");
+        // }
+
+
+
         
     }
 }
