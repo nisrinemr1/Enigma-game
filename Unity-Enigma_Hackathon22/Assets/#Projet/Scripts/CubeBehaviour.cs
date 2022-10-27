72675817cafafab6f5dc4c84a4229bd1b5b1668c
ShailaDevi Mahaux <shailadevi_mahaux@yahoo.fr>
2022-10-27 11:04:17 +0200

    added prefab button

--- Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/CubeBehaviour.cs
+++ Unity-Enigma_Hackathon22/Assets/#Projet/Scripts/CubeBehaviour.cs
@@ -12,11 +12,15 @@ public class CubeBehaviour : MonoBehaviour
 
     //Color32 va me donner en 32 bit le format rgb d'un objet
     public Color32 colorCube;
+    void Start()
+    {
+        colorCube = gameObject.GetComponent<MeshRenderer>().material.color;
+        transformCube.text = $"{transform.position}  \n {transform.localScale} \n {colorCube.r+ " "+colorCube.g+ " "+colorCube.b}";
+    }
     void Update()
     {
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-        colorCube = gameObject.GetComponent<MeshRenderer>().material.color;
-        transformCube.text = $"{transform.position}  \n {transform.localScale} \n {colorCube.r+ " "+colorCube.g+ " "+colorCube.b}";
+        
         
     }
 
