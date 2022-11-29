2de16097bdee79bcae3af5b109de9212f11d73c2
Nisrine Maruan <85900780+nisrinemr1@users.noreply.github.com>
2022-11-29 21:22:34 +0100

    re correct the new link

--- front/component/webgl.vue
+++ front/component/webgl.vue
@@ -1,7 +1,7 @@
 <template> 
    <!-- <Unity  width="1000" height="600" unityLoader="http://localhost:8888/group_project/i3_hackathon/webgl/Build/Build.loader.js"></Unity>  -->
 <!-- <iframe src="http://192.168.3.47/game-test/index.html" width="1000" height="600"></iframe> -->
-<iframe src="https://hackathon22.interface3.be/FinalGame/index.html" width="1000" height="600"></iframe>
+<iframe src="https://hackathon22.interface3.be/Enigma/FinalGame/index.html" width="1000" height="600"></iframe>
 
 </template>
 
